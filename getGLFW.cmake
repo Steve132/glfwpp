73e6ba3bb42b8e3527d5ac9f4d5c29e917925dbb
steve132 steve132 <steve@soapforge.com>
2021-05-26 17:37:18 -0400

    Fix this with the damn policy shit.

--- getGLFW.cmake
+++ getGLFW.cmake
@@ -38,11 +38,13 @@ if(NOT GLFW3_FOUND)
 	
 if((NOT EXISTS ${GLFW3_SOURCE_DIR}) AND GLFW3_TRY_FETCH)
 	include(FetchContent)
+	set(GLFW3_ZIP_URL "https://github.com/glfw/glfw/releases/download/${GLFW3_FETCH_VERSION}/glfw-${GLFW3_FETCH_VERSION}.zip")
 	FetchContent_Declare(glfw3 
-				URL "https://github.com/glfw/glfw/releases/download/${GLFW3_FETCH_VERSION}/glfw-${GLFW3_FETCH_VERSION}.zip"
+				URL ${GLFW3_ZIP_URL}
 				)
 	FetchContent_GetProperties(glfw3)
 	if(NOT glfw3_POPULATED)
+	  message(STATUS "Downloading ${GLFW3_ZIP_URL}")
 		FetchContent_Populate(glfw3)
 		if(glfw3_POPULATED)
 			set(GLFW3_SOURCE_DIR ${glfw3_SOURCE_DIR})
@@ -51,12 +53,10 @@ if((NOT EXISTS ${GLFW3_SOURCE_DIR}) AND GLFW3_TRY_FETCH)
 endif()
 if(EXISTS ${GLFW3_SOURCE_DIR})
 	include(ExternalProject)
-	set(UseDLLCRT OFF)
 	
-	if(MSVC AND (CMAKE_MSVC_RUNTIME_LIBARY_DLL MATCHES "DLL"))
-		set(UseDLLCRT ON)
-	endif()
-
+	#-DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON #this is broken.  It actually disables the patch and enables the default. it looks like it's copied from bullet.
+	# CMAKE_MSVC_RUNTIME_LIBARY=${CMAKE_MSVC_RUNTIME_LIBRARY} #pass through the default
+	#-DCMAKE_POLICY_DEFAULT_CMP<NNNN>=<OLD|NEW> set the policy to read the variable.
 	ExternalProject_Add(glfw3-external 
 		SOURCE_DIR ${GLFW3_SOURCE_DIR}
 		EXCLUDE_FROM_ALL
@@ -67,7 +67,9 @@ if(EXISTS ${GLFW3_SOURCE_DIR})
 			-DGLFW_INSTALL=ON
 			-DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/glfw3-external-prefix/install
 			-DBUILD_SHARED_LIBS=OFF
-			-DUSE_MSVC_RUNTIME_LIBRARY_DLL=${UseDLLCRT}
+			-DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON
+			-DCMAKE_MSVC_RUNTIME_LIBRARY=${CMAKE_MSVC_RUNTIME_LIBRARY}
+			-DCMAKE_POLICY_DEFAULT_CMP0091=NEW
 	)
 	add_library(glfw3 INTERFACE)
 	add_library(glfw3-ex UNKNOWN IMPORTED GLOBAL)
